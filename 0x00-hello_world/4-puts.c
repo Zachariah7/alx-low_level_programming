7dfb815b1ea563176566f59b1bbb65a08d162c35
Zachariah7 <z.yamine@gmail.com>
2023-03-16 18:09:42 -0700

    test commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
- * main is the entry point
- *This is a c code that prints a char using puts function
- *the return of the value 0 means that the execution has succeded
-*/
+/*
+ * Description: This is a c code that prints a char using puts function
+ * return: Always 0 (success)
+ */
 int main(void)
+/*
+ * main - entry point
+ */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-/**
- * main is the entry point
+/*
+ *int main(void): is the entry point
  *This is a c code that prints a char using printf function
- *the return of the value 0 means that the execution has succeded
+ *return: 0 means that the execution has succeded
 */
 int main(void)
+/*
+ *int main(void): is the entry point
+*/
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of a int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of an int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
 return (0);
 }
