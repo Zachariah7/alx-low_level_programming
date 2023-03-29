cc472cf4e7f3643695c137e32dd3746e776dd479
Zachariah7 <z.yamine@gmail.com>
2023-03-29 09:42:02 -0700

    commiting 0x02-functions_nested_loops tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int k;
+	
+	k = n % 10
+
+	if (n < 0)
+
+	k = -k;
+
+	_putchar(k + '0');
+
+	return (k);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the readme file of functions_nested_loops project
