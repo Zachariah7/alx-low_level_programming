05d3dd913dc6d8b4455e9cf988a3642d13f6e4cb
Zachariah7 <z.yamine@gmail.com>
2023-04-03 18:57:54 -0700

    commiting 0x07C_project

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: pointer
+ * @accept: pointer
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				n++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (n);
+		}
+
+	}
+	return (n);
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - function finds the first occurrence
+ * of the substring needle in the string haystack.
+ * @haystack: string
+ * @needle: string
+ * Return: a pointer to the beginning of the located substring, or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+        int i;
+        int j;
+
+        for (i = 0; haystack[i] != '\0'; i++)
+                {
+                for (j = 0; needle[j] != '\0'; j++)
+                        {
+                        if (haystack[i] == needle[j])
+
+                        return (haystack);
+                        }
+
+                }
+
+        return ('\0');
+}
