f9d6206523294e25f77eb5646210c7245fcdcbd9
Zachariah7 <z.yamine@gmail.com>
2023-04-17 17:05:40 -0700

    test2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,14 +20,13 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
 #endif
